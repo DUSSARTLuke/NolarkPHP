bcea195bcd0d1a5bd98f1a5719e768784ddef413
Luke DUSSART <lukedussart@hotmail.fr>
2020-04-10 11:37:28 +0200

    page admin finie

--- admin/admin.php
+++ admin/admin.php
@@ -3,14 +3,19 @@
     <head>
         <title>Casques Nolark : Sécurité et confort, nos priorités !</title>
         <meta charset="UTF-8">
-        <meta name="author" content="José GIL">
+        <meta name="author" content="Luke DUSSART">
         <meta name="description" content="Découvrez des casques moto dépassant même les exigences des tests de sécurité. Tous les casques Nolark au meilleur prix et avec en prime la livraison gratuite !">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="css/styles.css" rel="stylesheet" type="text/css">
-        <link rel="icon" href="favicon.ico">
+        <link href="../css/styles.css" rel="stylesheet" type="text/css">
+        <link rel="icon" href="../favicon.ico">
     </head>
     <body>
         <?php
-            include('./includes/header.html.inc.php');
+            include('../includes/headeradmin.inc.php');
         ?>
+        <?php
+            include('../includes/footer.inc.php');
+        ?>
+    </body>
+</html> 
 
