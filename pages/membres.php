4bfd6163a42dcdeace24c0adee01a3c1e972b013
Luke DUSSART <lukedussart@hotmail.fr>
2020-04-13 19:22:27 +0200

    page membres

--- pages/membres.php
+++ pages/membres.php
@@ -12,7 +12,7 @@
         <meta name="author" content="Luke DUSSART">
         <meta name="description" content="Découvrez des casques moto dépassant même les exigences des tests de sécurité. Tous les casques Nolark au meilleur prix et avec en prime la livraison gratuite !">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="../css/styles.css" rel="stylesheet" type="text/css">
+        <link href="../css/membres.css" rel="stylesheet" type="text/css">
         <link rel="icon" href="../favicon.ico">
     </head>
     <body>
@@ -20,8 +20,11 @@
             include('../includes/headeradmin.inc.php');
         ?>
          <form id="form_contact" name="form_contact" action="http://gil83.fr/nolark/testforms.php" method="POST">
-                <fieldset id="coordo">
-        <?php
+                <fieldset id="connexion">
+                    <legend>Pour mieux vous connaître</legend>
+                    <div><label for="i_pseudo">Votre pseudo :</label> <input type="text" name="i_pseudo" id="i_pseudo" size="35" required></div>
+                    <div><label for="i_passwd">Votre mot de passe :</label> <input type="password" name="i_passwd" id="i_passwd" size="35" required></div>
+                        <?php
             include('../includes/footer.inc.php');
         ?>
     </body>
