c7fec2b8fef593bbd9af8c31985d94a94ea22d1e
Luke DUSSART <lukedussart@hotmail.fr>
2020-04-15 12:04:05 +0200

    code inscription

--- includes/db.php
+++ includes/db.php
@@ -1,4 +1,4 @@
 <?php
 $cnx = new PDO('mysql:host=127.0.0.1;dbname=nolark', 'nolarkuser', 'nolarkpwd');
-$cnx ->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-$cnx ->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+$cnx->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$cnx->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
