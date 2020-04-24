942bbe4e586e38c9ba7f6069997b7cbe7539c068
Luke DUSSART <lukedussart@hotmail.fr>
2020-04-24 14:40:08 +0200

    page inscription finie (seul le captcha ne fonctionne pas)

--- includes/hautinscription.php
+++ includes/hautinscription.php
@@ -14,7 +14,7 @@ echo '<!DOCTYPE html>
         <meta name="description" content="Découvrez des casques moto dépassant même les exigences des tests de sécurité. Tous les casques Nolark au meilleur prix et avec en prime la livraison gratuite !">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="../css/styles.css" rel="stylesheet" type="text/css">
-        <link href="../css/inscription.css" rel="stylesheet" type="text/css">
+        <link href="../css/inscrit.css" rel="stylesheet" type="text/css">
         <link rel="icon" href="../favicon.ico">
     </head>';
 $queries = 0;
--- pages/inscription.php
+++ pages/inscription.php
@@ -28,13 +28,12 @@ $queries = 0;
                 <div><label for="date_naissance">Date de naissance : (format JJ/MM/AAAA)</label> <input type="text" name="date_naissance" id="date_naissance" size="30" /></div>
                 <div class="inscription"><input type="submit" value="Inscription" /></div>
             </fieldset>
-            <fieldset><legend>Protection anti-robot</legend>
+            <fieldset id="protect"><legend>Protection anti-robot</legend>
                 <p>Il est possible que certaines fois, l'image soit trop dure à lire ; le cas échéant, actualisez la page jusqu'à avoir une image lisible.</p>
                 <p>Si vous êtes dans l'incapacité de lire plusieurs images d'affilée, <a href="nous-contacter.php">contactez-nous</a>, nous nous occuperons de votre inscription.</p>
-                <label for="captcha" class="float">Entrez les 8 caractères (majuscules ou chiffres) contenus dans l'image :</label> <input type="text" name="captcha" id="captcha"><br/>
+                <label for="captcha">Entrez les 8 caractères (majuscules ou chiffres) contenus dans l'image :</label> <input type="text" name="captcha" id="captcha"><br/>
                 <img src="../pages/captcha.php" alt="image avec des caractères permettant de vérifier la non-automatisation" />
             </fieldset>
-            <div id="inscription"><input type="submit" value="Inscription" /></div>
         </form>
     </section>
     <?php
